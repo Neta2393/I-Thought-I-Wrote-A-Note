39bde33d75e93befb9f39283964036160758598b
Neta2393 <benetheahardin@yahoo.com>
2023-06-19 16:24:41 -0700

    Added port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require("http");
 
 
 const app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));

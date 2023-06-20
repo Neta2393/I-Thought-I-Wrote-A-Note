ee9e21cb59af2cc37df6e1147275427f1813e1e4
Neta2393 <benetheahardin@yahoo.com>
2023-06-19 17:12:23 -0700

    Added file and context in js files

--- server.js
+++ server.js
@@ -1,55 +1,25 @@
-const express = require("express");
-const fs = require("fs");
-const notes = require("db.json");
-const path = require("path");
-const http = require("http");
+// Dependencies 
+const express = require('express');
 
+// Direct server to use routes
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlroutes.js');
 
-
+// Create server
 const app = express();
-var PORT = process.env.PORT || 8080;
 
-// Middleware
+// Set PORT
+const PORT = process.env.PORT || 3001;
+
+// Middleware to complete request - response cycle
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-//Setting routes for APIs
-//This gets notes saved and joins it in db.json
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "db.json"))
-});
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// Post function to add new notes to db.json
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("db.json"));
-    const newNotes = req.body;
-    newNotes.id = uuid.v4();
-    notes.push(newNotes);
-    fs.writeFileSync("db.json", JSON.stringify(notes))
-    res.json(notes);
-});
-
-//used for deleting notes
-app.delete("/api/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("db.json"));
-    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-    fs.writeFileSync("db.json", JSON.stringify(delNote));
-    res.json(delNote);
-})
-
-
-//HTML calls
-//calls home page
-app.get("db.json", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-//call for notes.html
-app.get("public\assets\notes.html", function (req, res) {
-    res.sendFile(path.join(__dirname, "public\assets\notes.html"));
-});
-
-//Start listen
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+// Listener
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${3001}!`);
 });

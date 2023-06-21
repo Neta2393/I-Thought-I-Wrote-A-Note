c6603c66198848a49f964ed2a95b6eac03120706
Neta2393 <benetheahardin@yahoo.com>
2023-06-20 21:36:44 -0700

    Added Route

--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 const path =  require('path');
 
 // GET routes for possible query parameters - intended for HTML page
-router.get('/', (req, res) => { 
+router.get('public\routes\htmlRoutes.js', (req, res) => { 
     res.sendFile(path.join(__dirname, 'public\notes.html'));
 });
 
-router.get('/notes', (req, res) => { 
+router.get('public\routes\htmlRoutes.js', (req, res) => { 
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
-router.get('*', (req, res) => { 
+router.get('public\routes\htmlRoutes.js', (req, res) => { 
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
